9d22711b856454b879f6a83854afec20ee2f86cf
Edoardo <edoardo.costantini@prima.it>
2021-10-07 18:01:45 +0200

    added tests

--- apps/largestfivedigits/test/largestfivedigits_test.exs
+++ apps/largestfivedigits/test/largestfivedigits_test.exs
@@ -2,7 +2,11 @@ defmodule LargestfivedigitsTest do
   use ExUnit.Case
   doctest Largestfivedigits
 
-  test "greets the world" do
-    assert Largestfivedigits.hello() == :world
+  test "largest five digits" do
+    assert Largestfivedigits.largest_five_digits(nil) != 0
+    assert Largestfivedigits.largest_five_digits(nil) != nil
+    assert Largestfivedigits.largest_five_digits(nil) == 99890
+    assert Largestfivedigits.largest_five_digits("1234567898765" == 98765)
+    assert Largestfivedigits.largest_five_digits("731674765" == 74765)
   end
 end
