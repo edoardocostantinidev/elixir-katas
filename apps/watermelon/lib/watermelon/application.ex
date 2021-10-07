2bf14a017b28045161201fa08ba664447ca0d9c3
Edoardo <edoardo.costantini@prima.it>
2021-10-07 11:48:30 +0200

    added files... duh

---
+++ apps/watermelon/lib/watermelon/application.ex
@@ -0,0 +1,20 @@
+defmodule Watermelon.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Watermelon.Worker.start_link(arg)
+      # {Watermelon.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Watermelon.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
